04106958cf7bc6e0c5fc624e2ef3b6bce22c8739
Bernadette Theuretzbachner <bernadette.theuretzbachner@jumio.com>
2020-04-10 11:48:15 +0200

    IPHONE-6870: Add camera permissions.

--- ios/SafariViewController-Pilot/SafariViewController-Pilot/ViewController.swift
+++ ios/SafariViewController-Pilot/SafariViewController-Pilot/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Bernadette Theuretzbachner on 10.04.20.
 //  Copyright © 2020 Bernadette Theuretzbachner. All rights reserved.
 //
+import AVFoundation
 import SafariServices
 import UIKit
 
@@ -14,22 +15,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputField: UITextField!
     
     @IBAction func loadButton(_ sender: Any) {
+        checkCameraPermission() 
         let inputText: String = inputField.text!
         showSafariVC(inputText)
     }
     
+    // present SFSafariViewController
     private func showSafariVC(_ stringURL: String) {
         guard let URL = URL(string: stringURL) else {
             return
         }
 
-        // Present SFSafariViewController
         let safariVC = SFSafariViewController(url: URL)
         present(safariVC, animated: true)
     }
         
     func safariViewControllerDidFinish(_ safariVC: SFSafariViewController) {
         safariVC.dismiss(animated: true, completion: nil)
+    }
+    
+    // ask for camera permissions
+    func checkCameraPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            if !granted {
+                print("Camera permission denied")
+            }
+        }
     }
 }
 
